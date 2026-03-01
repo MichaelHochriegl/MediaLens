9ffda2507b9a61b8ded23a3a9111ea443d4c6226
MichaelHochriegl <michael@hochriegl.dev>
2026-03-01 14:14:06 +0100

    Remove unused `MediaInfoHandle.Create` method from `MediaInfoHandle`.

--- src/MediaLens/Native/MediaInfoHandle.cs
+++ src/MediaLens/Native/MediaInfoHandle.cs
@@ -4,17 +4,6 @@ namespace MediaLens.Native;
 
 internal sealed class MediaInfoHandle() : SafeHandleZeroOrMinusOneIsInvalid(ownsHandle: true)
 {
-    // internal static MediaInfoHandle Create()
-    // {
-    //     var ptr = MediaInfoNative.New();
-    //     if (ptr == IntPtr.Zero)
-    //         throw new InvalidOperationException("Failed to create MediaInfo handle.");
-    //
-    //     var h = new MediaInfoHandle();
-    //     h.SetHandle(ptr);
-    //     return h;
-    // }
-
     protected override bool ReleaseHandle()
     {
         MediaInfoNative.Delete(handle);
