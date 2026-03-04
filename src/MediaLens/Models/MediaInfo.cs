128ab8cd07b662259792d18d7d319521b87414c8
MichaelHochriegl <michael@hochriegl.dev>
2026-03-04 20:05:31 +0100

    Rename `Videos`, `Audio`, and `Text` properties in `MediaInfo` to `VideoTracks`, `AudioTracks`, and `TextTracks` for consistency.

--- src/MediaLens/Models/MediaInfo.cs
+++ src/MediaLens/Models/MediaInfo.cs
@@ -2,7 +2,7 @@ namespace MediaLens.Models;
 
 public sealed record MediaInfo(
     GeneralTrack General,
-    IReadOnlyList<VideoTrack> Videos,
-    IReadOnlyList<AudioTrack> Audio,
-    IReadOnlyList<TextTrack> Text
+    IReadOnlyList<VideoTrack> VideoTracks,
+    IReadOnlyList<AudioTrack> AudioTracks,
+    IReadOnlyList<TextTrack> TextTracks
 );
